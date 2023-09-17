b627d098ce86ff7e3191bc197d06f0d6a9b69d2c
Pedro Perez <75536176+pperez303@users.noreply.github.com>
2023-09-17 13:10:37 -0700

    added header component to About and Contact pages

--- frontend-react/src/pages/about/About.js
+++ frontend-react/src/pages/about/About.js
@@ -1,45 +1,51 @@
 //import React from 'react';  
 import "./about.css";
 import neo from "../../images/neo.jpg"
+
+//components
 import Sidebar from "../../components/sidebar/Sidebar";
+import Header from "../../components/header/Header";
 
 function About () {  
     return (
-    <main className="main">
-        <div className="aboutsection">
-            <h2>About</h2>
-            <section className="about">
-                <header className="aboutHeader">
-                    <img className = "aboutImg" src={neo} alt="" />
-                    <div className="aboutInfo">
-                        <h3>Peter Perez</h3>
-                        <p>Technical Project Manager</p>
-                    </div>
-                </header>
-                <p className="aboutDesc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Modi laudantium atque quisquam, perferendis id maxime 
-                    aliquam praesentium numquam ipsum debitis, eligendi 
-                    aspernatur hic tempora ab. Quidem non exercitationem sit porro!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Nesciunt ab suscipit ducimus! Quod libero eligendi esse 
-                    similique praesentium neque rem, assumenda rerum nesciunt 
-                    alias voluptatem cum quo reprehenderit, voluptas sed.
-                </p>
-            </section>
-            
-            <section className="MERN">
-                <h3>MERN</h3>
-                <p>MongoDB</p>
-                <p>ExpressJS</p>
-                <p>ReactJS</p>
-                <p>NodeJS</p>
-            </section>
-        </div>
-        <Sidebar className="sidesection"/>
-    </main>
+    <>
+        <Header />
+        <main className="main">            
+            <div className="aboutsection">
+                <h2>About</h2>
+                <section className="about">
+                    <header className="aboutHeader">
+                        <img className = "aboutImg" src={neo} alt="" />
+                        <div className="aboutInfo">
+                            <h3>Peter Perez</h3>
+                            <p>Technical Project Manager</p>
+                        </div>
+                    </header>
+                    <p className="aboutDesc">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Modi laudantium atque quisquam, perferendis id maxime 
+                        aliquam praesentium numquam ipsum debitis, eligendi 
+                        aspernatur hic tempora ab. Quidem non exercitationem sit porro!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Nesciunt ab suscipit ducimus! Quod libero eligendi esse 
+                        similique praesentium neque rem, assumenda rerum nesciunt 
+                        alias voluptatem cum quo reprehenderit, voluptas sed.
+                    </p>
+                </section>
+                
+                <section className="MERN">
+                    <h3>MERN</h3>
+                    <p>MongoDB</p>
+                    <p>ExpressJS</p>
+                    <p>ReactJS</p>
+                    <p>NodeJS</p>
+                </section>
+            </div>
+            <Sidebar className="sidesection"/>
+        </main>
+    </>
     )
 }  
 export default About;  
--- frontend-react/src/pages/contact/Contact.js
+++ frontend-react/src/pages/contact/Contact.js
@@ -1,38 +1,44 @@
 import "./contact.css";
 import neo from "../../images/neo.jpg"
+
+// Components
 import Sidebar from "../../components/sidebar/Sidebar";
+import Header from "../../components/header/Header";
 
 function Contact () {  
     return (
-    <main className="main">
-        <div className="contactsection">
-            <h2>Contact me</h2>
-            <section className="contact">
-                <header className="contactHeader">
-                    <img className = "contactImg" src={neo} alt="" />
-                    <div className="contactInfo">
-                        <h3>Peter Perez</h3>
-                        <p>Technical Project Manager</p>
-                    </div>
-                </header>
-                <p className="contact_Phone">
-                    1-702-468-8210
-                </p>
-                <p className="contact_Email">
-                    peteperez.lv@gmail.com
-                </p>
-            </section>
-            
-            <section className="MERN">
-                <h3>MERN</h3>
-                <p>MongoDB</p>
-                <p>ExpressJS</p>
-                <p>ReactJS</p>
-                <p>NodeJS</p>
-            </section>
-        </div>
-        <Sidebar className="sidebar" />
-    </main>
+    <>
+        <Header />
+        <main className="main">
+            <div className="contactsection">
+                <h2>Contact me</h2>
+                <section className="contact">
+                    <header className="contactHeader">
+                        <img className = "contactImg" src={neo} alt="" />
+                        <div className="contactInfo">
+                            <h3>Peter Perez</h3>
+                            <p>Technical Project Manager</p>
+                        </div>
+                    </header>
+                    <p className="contact_Phone">
+                        1-702-468-8210
+                    </p>
+                    <p className="contact_Email">
+                        peteperez.lv@gmail.com
+                    </p>
+                </section>
+                
+                <section className="MERN">
+                    <h3>MERN</h3>
+                    <p>MongoDB</p>
+                    <p>ExpressJS</p>
+                    <p>ReactJS</p>
+                    <p>NodeJS</p>
+                </section>
+            </div>
+            <Sidebar className="sidebar" />
+        </main>
+    </>
     )
 }  
 export default Contact;  
